6a73e3aa83a081953f4b56d5db3c00c1c4df0126
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-10-16 15:42:09 +0200

    Change types to pascal case

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,10 @@
-export type pomodoroTime = { minutes: number; seconds: number }
+export type PomodoroTime = { minutes: number; seconds: number }
 
-export type pomodoroHtmlElements = {
+export type PomodoroHtmlElements = {
   displayElement: HTMLElement
   startButton: HTMLElement
   pauseButton: HTMLElement
   resetButton: HTMLElement
   minutesInput: HTMLInputElement
+  submitMinutesButton: HTMLElement
 }
