22de352d54b6b26e51a625edcc67a26dd098baf6
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-10-16 15:44:50 +0200

    Refactor initialization for easier handling of consent mesages

--- src/PomodoroApp.ts
+++ src/PomodoroApp.ts
@@ -1,29 +1,63 @@
 import { TimerDisplay } from './TimerDisplay'
 import { Validator } from './Validator'
-import type { pomodoroHtmlElements } from './types'
+import type { PomodoroHtmlElements } from './types'
 
 export class PomodoroApp {
-  #timerDisplay: TimerDisplay
+  #timerDisplay!: TimerDisplay
 
-  #startButton: HTMLElement
-  #pauseButton: HTMLElement
-  #resetButton: HTMLElement
+  #submitMinutesButton!: HTMLElement
 
-  #inputMinutes: HTMLElement
+  #startButton!: HTMLElement
+  #pauseButton!: HTMLElement
+  #resetButton!: HTMLElement
+
+  #inputMinutes!: HTMLElement
 
   #validator = new Validator()
 
-  constructor(htmlElements: pomodoroHtmlElements) {
-    this.#validateHtmlElements(htmlElements)
+  constructor(htmlContainer: HTMLElement) {
+    // this.#validateHtmlElements(htmlElements)
+    this.#validator.validateHtmlElement(htmlContainer)
 
+    this.#initializeRequiredHtmlElements(htmlContainer)
+
+    this.#testApp()
+  }
+
+  #initializeRequiredHtmlElements(htmlElement: HTMLElement) {
+    const pomodoroHtmlElements: PomodoroHtmlElements = {
+      displayElement: htmlElement.querySelector(
+        '#pomodoro-display-element'
+      ) as HTMLElement,
+      startButton: htmlElement.querySelector(
+        '#pomodoro-startButton-element'
+      ) as HTMLElement,
+      pauseButton: htmlElement.querySelector(
+        '#pomodoro-pauseButton-element'
+      ) as HTMLElement,
+      resetButton: htmlElement.querySelector(
+        '#pomodoro-resetButton-element'
+      ) as HTMLElement,
+      minutesInput: htmlElement.querySelector(
+        '#pomodoro-input-element'
+      ) as HTMLInputElement,
+      submitMinutesButton: htmlElement.querySelector(
+        '#pomodoro-submit-minutes'
+      ) as HTMLButtonElement
+    }
+
+    this.#validateHtmlElements(pomodoroHtmlElements)
+
+    this.#assignHtmlElementsToPrivateFields(pomodoroHtmlElements)
+  }
+
+  #assignHtmlElementsToPrivateFields(htmlElements: PomodoroHtmlElements) {
     this.#startButton = htmlElements.startButton
     this.#pauseButton = htmlElements.pauseButton
     this.#resetButton = htmlElements.resetButton
     this.#inputMinutes = htmlElements.minutesInput
 
     this.#timerDisplay = new TimerDisplay(htmlElements.displayElement)
-
-    this.#testApp()
   }
 
   #testApp() {
@@ -31,7 +65,7 @@ export class PomodoroApp {
     this.#timerDisplay.update({ minutes: 22, seconds: 62 })
   }
 
-  #validateHtmlElements(htmlElements: pomodoroHtmlElements) {
+  #validateHtmlElements(htmlElements: PomodoroHtmlElements) {
     for (const htmlElement of Object.values(htmlElements)) {
       this.#validator.validateHtmlElement(htmlElement)
     }
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,6 @@
 import { PomodoroApp } from './PomodoroApp'
 import './styles/styles.css'
-import type { pomodoroHtmlElements } from './types'
 
-const pomodoroHtmlElements: pomodoroHtmlElements = {
-  displayElement: document.querySelector(
-    '#pomodoro-display-element'
-  ) as HTMLElement,
-  startButton: document.querySelector(
-    '#pomodoro-startButton-element'
-  ) as HTMLElement,
-  pauseButton: document.querySelector(
-    '#pomodoro-pauseButton-element'
-  ) as HTMLElement,
-  resetButton: document.querySelector(
-    '#pomodoro-resetButton-element'
-  ) as HTMLElement,
-  minutesInput: document.querySelector(
-    '#pomodoro-input-element'
-  ) as HTMLInputElement
-}
-
-new PomodoroApp(pomodoroHtmlElements)
+new PomodoroApp(
+  document.querySelector('#pomodoro-app-container') as HTMLElement
+)
